de65b79529fccfb980aaef2e52821b0ec69a246b
Andrei Fidelman <fidelman-life@hotmail.com>
2016-11-04 03:42:07 +0500

    gulp.series('task')

--- gulpfile.js
+++ gulpfile.js
@@ -6,3 +6,14 @@ gulp.task('hello', function(callback) {
     console.log('hi');
     callback(); // the task has finished
 });
+
+gulp.task('deals', function(callback) {
+    console.log('how are you?');
+    callback();
+});
+
+// call task sync
+
+gulp.task('greeting', gulp.series('hello', 'deals'));
+
+// gulp does not pass data among tasks
