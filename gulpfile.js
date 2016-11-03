402a5a7cebdbdaaac267bf61ff48b6651c26153d
Andrei Fidelman <fidelman-life@hotmail.com>
2016-11-04 04:06:14 +0500

    copy files to the new directory

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,7 @@
 
 const gulp = require('gulp');
 
-gulp.task('hello', function(callback) {
-    console.log('hi');
-    callback(); // the task has finished
+gulp.task('default', function() {
+    return gulp.src('source/**/*.*')
+        .pipe(gulp.dest('dest'));
 });
-
-gulp.task('deals', function(callback) {
-    console.log('how are you?');
-    callback();
-});
-
-// call task async
-
-gulp.task('greeting', gulp.parallel('hello', 'deals'));
-
-// gulp does not pass data among tasks
