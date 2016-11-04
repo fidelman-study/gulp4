7866d878fee02a5b2363c18a344cf39a1f70b421
Andrei Fidelman <fidelman-life@hotmail.com>
2016-11-04 21:28:21 +0500

    make order by preproc

--- gulpfile.js
+++ gulpfile.js
@@ -7,11 +7,7 @@ const debug = require('gulp-debug');
 
 
 gulp.task('styles', function() {
-    return gulp.src('frontend/styles/**/*.styl')
-        .pipe(debug({title: 'src'}))
+    return gulp.src('frontend/styles/main.styl')
         .pipe(stylus())
-        .pipe(debug({title: 'stylus'}))
-        .pipe(concat('all.css'))
-        .pipe(debug({title: 'concat'}))
         .pipe(gulp.dest('public'));
 });
