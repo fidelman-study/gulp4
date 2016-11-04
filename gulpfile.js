2a8fe8022a5d69bf280b2fbc185751c5ced23e78
Andrei Fidelman <fidelman-life@hotmail.com>
2016-11-05 02:18:26 +0500

    add autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,18 @@
 'use strict';
 
 const gulp = require('gulp');
-const stylus = require('gulp-stylus');
-const sourcemaps = require('gulp-sourcemaps');
-const gulpIf = require('gulp-if');
-const del = require('del');
-const debug = require('gulp-debug');
-const newer = require('gulp-newer');
-
-const isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
+const autoprefixer = require('gulp-autoprefixer');
+const concat = require('gulp-concat');
 
 gulp.task('styles', function() {
-    return gulp.src('frontend/styles/main.styl')
-        .pipe(gulpIf(isDevelopment, sourcemaps.init()))
-        .pipe(stylus())
-        .pipe(gulpIf(isDevelopment, sourcemaps.write()))
-        .pipe(gulp.dest('public'));
-});
-
-gulp.task('assets', function() {
-   return gulp.src('frontend/assets/**', {since: gulp.lastRun('assets')}) // работать с файлами которые изменились с последнего запуска
-       .pipe(newer('public'))
+   return gulp.src('frontend/styles/**/*.css')
+       .pipe(autoprefixer())
+       .pipe(concat('all.css'))
        .pipe(gulp.dest('public'));
 });
 
-gulp.task('clean', function() {
-    return del('public');
-});
-
-gulp.task('build', gulp.series('clean', gulp.parallel('styles', 'assets')));
-
 gulp.task('watch', function() {
-    gulp.watch('frontend/styles/**/*.*', gulp.series('styles'));
-    gulp.watch('frontend/assets/**/*.*', gulp.series('assets'));
+   gulp.watch('frontend/styles/**/*.css', gulp.series('styles'));
 });
 
-gulp.task('dev', gulp.series('build', 'watch'));
-
+gulp.task('dev', gulp.series('styles', 'watch'));
