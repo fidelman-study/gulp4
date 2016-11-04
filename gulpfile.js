4e99ed454109b587e7b3e31646a610999a4d3334
Andrei Fidelman <fidelman-life@hotmail.com>
2016-11-04 22:01:54 +0500

    add gulp-if plugin

--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,14 @@
 const gulp = require('gulp');
 const stylus = require('gulp-stylus');
 const sourcemaps = require('gulp-sourcemaps');
+const gulpIf = require('gulp-if');
 
 const isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
 
 gulp.task('styles', function() {
-    let pipeline = gulp.src('frontend/styles/main.styl');
-
-    if(isDevelopment) pipeline = pipeline.pipe(sourcemaps.init());
-    pipeline.pipe(stylus());
-
-    if(isDevelopment) pipeline = pipeline.pipe(sourcemaps.write());
-
-    return pipeline.pipe(gulp.dest('public'));
+    return gulp.src('frontend/styles/main.styl')
+        .pipe(gulpIf(isDevelopment, sourcemaps.init()))
+        .pipe(stylus())
+        .pipe(gulpIf(isDevelopment, sourcemaps.write()))
+        .pipe(gulp.dest('public'));
 });
