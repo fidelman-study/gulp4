169ddb98de39caef587ecf2971d1cd3e022f50a2
Andrei Fidelman <fidelman-life@hotmail.com>
2016-11-04 21:39:20 +0500

    add sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,13 @@
 
 const gulp = require('gulp');
 const stylus = require('gulp-stylus');
-const concat = require('gulp-concat');
-const debug = require('gulp-debug');
+const sourcemaps = require('gulp-sourcemaps');
 
 
 gulp.task('styles', function() {
     return gulp.src('frontend/styles/main.styl')
+        .pipe(sourcemaps.init())
         .pipe(stylus())
+        .pipe(sourcemaps.write()) // можно внутри указать каталог для отдельного файла
         .pipe(gulp.dest('public'));
 });
