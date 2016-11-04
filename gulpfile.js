4f0a5c362c7ebc6a492bec58ddb75d326a49eb40
Andrei Fidelman <fidelman-life@hotmail.com>
2016-11-04 20:57:55 +0500

    add concat plugin

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,12 @@
 
 const gulp = require('gulp');
 const stylus = require('gulp-stylus');
+const concat = require('gulp-concat');
 
 
 gulp.task('styles', function() {
     return gulp.src('frontend/styles/**/*.styl')
         .pipe(stylus())
+        .pipe(concat('all.css'))
         .pipe(gulp.dest('public'));
 });
