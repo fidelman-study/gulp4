96d2f0f119da99ecd38be1117a4b21b683d925e9
Andrei Fidelman <fidelman-life@hotmail.com>
2016-11-05 02:46:08 +0500

    cached instead of since

--- gulpfile.js
+++ gulpfile.js
@@ -5,9 +5,11 @@ const autoprefixer = require('gulp-autoprefixer');
 const concat = require('gulp-concat');
 const remember = require('gulp-remember');
 const path = require('path');
+const cached = require('gulp-cached');
 
 gulp.task('styles', function() {
-   return gulp.src('frontend/styles/**/*.css', {since: gulp.lastRun('styles')})
+   return gulp.src('frontend/styles/**/*.css')
+       .pipe(cached('styles')) // разница с since в том, что он смотрит только на содержание
        .pipe(autoprefixer())
        .pipe(remember('styles')) // запоминает все прохожденные через него файлы, работает как связка с since
        .pipe(concat('all.css'))
@@ -16,8 +18,9 @@ gulp.task('styles', function() {
 
 gulp.task('watch', function() {
    gulp.watch('frontend/styles/**/*.css', gulp.series('styles'))
-       .on('unlink', function(filepath) { // обработчик при удалении файла
-          remember.forget('styles', path.resolve(filepath)); //Нужен абсолютный путь, поэтому нужен модуль path
+       .on('unlink', function(filepath) {
+           remember.forget('styles', path.resolve(filepath)); // удаляет из модуля remember
+           delete cached.cashes.styles[path.resolve(filepath)]; // удаляет из модуля cached
        });
 });
 
