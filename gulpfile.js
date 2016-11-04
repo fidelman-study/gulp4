4cea561e00fad402914c2705930382c241bf4579
Andrei Fidelman <fidelman-life@hotmail.com>
2016-11-04 22:13:23 +0500

    сopy to the resources route

--- gulpfile.js
+++ gulpfile.js
@@ -16,8 +16,13 @@ gulp.task('styles', function() {
         .pipe(gulp.dest('public'));
 });
 
+gulp.task('assets', function() {
+   return gulp.src('frontend/assets/**')
+       .pipe(gulp.dest('public'))
+});
+
 gulp.task('clean', function() {
     return del('public');
 });
 
-gulp.task('build', gulp.series('clean', 'styles'));
+gulp.task('build', gulp.series('clean', gulp.parallel('styles', 'assets')));
