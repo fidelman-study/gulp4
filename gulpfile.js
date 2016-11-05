264dc08b056d1b4e73f871a3e1f11ddfcce755da
Andrei Fidelman <fidelman-life@hotmail.com>
2016-11-05 16:56:04 +0500

    throught2, wrapper to create node modules

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,12 @@
-
 'use strict';
 
 const gulp = require('gulp');
-const stylus = require('gulp-stylus');
-const sourcemaps = require('gulp-sourcemaps');
-const gulpIf = require('gulp-if');
-const del = require('del');
-const debug = require('gulp-debug');
-const notify = require('gulp-notify');
-const plumber = require('gulp-plumber');
-const combiner = require('stream-combiner2').obj;
-
-const isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
-
-gulp.task('styles', function() {
-    return combiner(
-        gulp.src('frontend/styles/main.styl'),
-        gulpIf(isDevelopment, sourcemaps.init()),
-        stylus(),
-        gulpIf(isDevelopment, sourcemaps.write()),
-        gulp.dest('public')
-    ).on('error', notify.onError())
-});
+const through2 = require('through2').obh;
 
 gulp.task('assets', function() {
-    return gulp.src('frontend/assets/**', {since: gulp.lastRun('assets')}) // работать с файлами которые изменились с последнего запуска
-        .pipe(debug())
-        .pipe(gulp.dest('public'));
+   return gulp.src('frontend/assets/**/*.*')
+       .pipe(throught2(function(file, enc, callback) {
+
+       }))
+       .pipe(gulp.dest('public'));
 });
-
-gulp.task('clean', function() {
-    return del('public');
-});
-
-gulp.task('build', gulp.series('clean', gulp.parallel('styles', 'assets')));
-
-gulp.task('watch', function() {
-    gulp.watch('frontend/styles/**/*.*', gulp.series('styles'));
-    gulp.watch('frontend/assets/**/*.*', gulp.series('assets'));
-});
-
-gulp.task('dev', gulp.series('build', 'watch'));
