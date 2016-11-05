81dcda6c5ccf6d48cac118b33ebe612f661c1213
Andrei Fidelman <fidelman-life@hotmail.com>
2016-11-05 17:01:59 +0500

    create plugin to clone file and add new extension

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,15 @@
 'use strict';
 
 const gulp = require('gulp');
-const through2 = require('through2').obh;
+const through2 = require('through2').obj;
 
 gulp.task('assets', function() {
    return gulp.src('frontend/assets/**/*.*')
-       .pipe(throught2(function(file, enc, callback) {
-
+       .pipe(through2(function(file, enc, callback) {
+          let file2 = file.clone(); // клонируем файл
+          file2.path += '.bak'; // добавляем новое расширение
+          this.push(file2); // добавляем его в поток
+          callback(null, file);
        }))
        .pipe(gulp.dest('public'));
 });
