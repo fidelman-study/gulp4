113802db1ff6e32b8302664b3e5c8dd61b4bb868
Andrei Fidelman <fidelman-life@hotmail.com>
2016-11-05 19:00:04 +0500

    simple eslint

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,10 @@
 'use strict';
 
 const gulp = require('gulp');
-const through2 = require('through2').obj;
-const File = require('vinyl');
+const eslint = require('gulp-eslint');
 
-gulp.task('assets', function() {
-
-    const mtimes = {}; // keep modification date
-
-    return gulp.src('frontend/assets/**/*.*')
-        .pipe(through2( // работа с файлами
-            function(file, enc, callback) {
-                mtimes[file.relative] = file.stat.mtime;
-                callback(null, file);
-            },
-            function(callback) { // генерация
-                let manifest = new File({ // создаем новый файл, который хранит в себе даты модификаций каждого файла
-                    contents: new Buffer(JSON.stringify(mtimes)),
-                    base: process.cwd(), // текущая директория
-                    path: process.cwd() + '/manifest.json'
-                });
-                manifest.isManifest = true;
-                this.push(manifest);
-                callback();
-            }
-        ))
-        .pipe(gulp.dest(function(file) {
-            if(file.isManifest) {
-                return process.cwd();
-            } else {
-                return 'public';
-            }
-        }));
+gulp.task('lint', function() {
+    return gulp.src('frontend/**/*.js')
+        .pipe(eslint())
+        .pipe(eslint.format())
 });
