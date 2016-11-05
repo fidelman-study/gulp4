d2afeeb95f734b1d553d89b0a618a35a1c90382c
Andrei Fidelman <fidelman-life@hotmail.com>
2016-11-05 17:33:25 +0500

    create file with files modification dates

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,34 @@
 
 const gulp = require('gulp');
 const through2 = require('through2').obj;
+const File = require('vinyl');
 
 gulp.task('assets', function() {
-   return gulp.src('frontend/assets/**/*.*')
-       .pipe(through2(function(file, enc, callback) {
-          let file2 = file.clone(); // клонируем файл
-          file2.path += '.bak'; // добавляем новое расширение
-          this.push(file2); // добавляем его в поток
-          callback(null, file);
-       }))
-       .pipe(gulp.dest('public'));
+
+    const mtimes = {}; // keep modification date
+
+    return gulp.src('frontend/assets/**/*.*')
+        .pipe(through2( // работа с файлами
+            function(file, enc, callback) {
+                mtimes[file.relative] = file.stat.mtime;
+                callback(null, file);
+            },
+            function(callback) { // генерация
+                let manifest = new File({ // создаем новый файл, который хранит в себе даты модификаций каждого файла
+                    contents: new Buffer(JSON.stringify(mtimes)),
+                    base: process.cwd(), // текущая директория
+                    path: process.cwd() + '/manifest.json'
+                });
+                manifest.isManifest = true;
+                this.push(manifest);
+                callback();
+            }
+        ))
+        .pipe(gulp.dest(function(file) {
+            if(file.isManifest) {
+                return process.cwd();
+            } else {
+                return 'public';
+            }
+        }));
 });
