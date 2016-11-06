9f3e1ddb0e7f4b43262a08f47f45d4c94f6c260a
Andrei Fidelman <fidelman-life@hotmail.com>
2016-11-06 11:15:59 +0500

    node.js stream

--- gulpfile.js
+++ gulpfile.js
@@ -1,53 +1,17 @@
 'use strict';
 
 const gulp = require('gulp');
-const eslint = require('gulp-eslint');
 const through2 = require('through2').obj;
-const fs = require('fs');
-const gulpIf = require('gulp-if');
-const combine = require('stream-combiner2').obj;
-const debug = require('gulp-debug');
 
-gulp.task('lint', function() {
-
-    let eslintResults = {};
-
-    let cachedFilePath = process.cwd() + '/tmp/lintCache.json';
-
-    try {
-        eslintResults = JSON.parse(fs.readFileSync(cachedFilePath));
-    } catch(e) {
-
-    }
-
-    return gulp.src('frontend/**/*.js', {read: false})
-        .pipe(debug({title: 'src'}))
-        .pipe(gulpIf(function(file) {
-                return eslintResults[file.path] && eslintResults[file.path].mtime == file.stat.mtime.toJSON();
-            },
-            through2(function(file, enc, callback) {
-                file.eslint = eslintResults[file.path].eslint;
-                callback(null, file);
-            }),
-            combine(
-                through2(function(file, enc, callback) {
-                    file.contents = fs.readFileSync(file.path);
-                    callback(null, file);
-                }),
-                eslint(),
-                debug({title: 'eslint'}),
-                through2(function (file, enc, callback) {
-                    eslintResults[file.path] = {
-                        eslint: file.eslint,
-                        mtime: file.stat.mtime
-                    };
-                    callback(null, file);
-                })
-            )
+gulp.task('default', function(callback) {
+    return gulp.src('node_modules/**/*.*')
+        .pipe(through2( // у pipe есть буфер в 16 элементов, при его заполнении, он останавливается
+            // необходимо восстановить его после обработки .resume() либо всегда возвращать gulp.src
+            function(file, enc, cb) {
+                console.log(file.relative);
+                cb(null, file);
+            }
         ))
-        .pipe(eslint.format())
-        .on('end', function(callback) {
-            fs.writeFileSync(cachedFilePath, JSON.stringify(eslintResults));
-        })
-
+        // .resume()
+        .on('end', callback);
 });
