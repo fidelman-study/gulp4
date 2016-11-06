7dfb4c6062fe9434ba4622b3accabe5a31b7472d
Andrei Fidelman <fidelman-life@hotmail.com>
2016-11-06 16:33:08 +0500

    production mode

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,54 @@
-
 'use strict';
+
 const gulp = require('gulp');
+const path = require('path');
+const del = require('del');
+const debug = require('gulp-debug');
+const sourcemaps = require('gulp-sourcemaps');
+const stylus = require('gulp-stylus');
+const browserSync = require('browser-sync').create();
+const cssnano = require('gulp-cssnano');
+const gulpIf = require('gulp-if');
+const rev = require('gulp-rev');
+const combine = require('stream-combiner2').obj;
+const revReplace = require('gulp-rev-replace');
 
-function lazyRequireTask(taskName, path, options) { // универсальная функция для вызова задач из файлов
-    options = options || {};
+const isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV == 'development';
 
-    options.taskName = taskName;
-    gulp.task(taskName, function(callback) {
-        let task = require(path).call(this, options);
-        return task(callback);
-    });
-}
-
-lazyRequireTask('styles', './tasks/styles.js', {
-    src: 'frontend/styles/main.styl'
+gulp.task('styles', function() {
+    return gulp.src('frontend/styles/index.styl')
+        .pipe(gulpIf(isDevelopment, sourcemaps.init()))
+        .pipe(stylus())
+        .pipe(gulpIf(isDevelopment, sourcemaps.write()))
+        .pipe(gulpIf(!isDevelopment, combine( cssnano(), rev() )))
+        .pipe(gulp.dest('public/styles'))
+        .pipe(gulpIf(!isDevelopment, combine( rev.manifest('css.json'), gulp.dest('manifest'))));
 });
+
+gulp.task('clean', function() {
+    return del('public');
+});
+
+gulp.task('assets', function() {
+    return gulp.src('frontend/assets/**/*.*', {since: gulp.lastRun('assets')})
+        .pipe(gulpIf(!isDevelopment(revReplace({ // условие при revReplace не работает
+            manifest: gulp.src('manifest/css.json', { allowEmpty: true }) // не ругаться если нету
+        })))
+        .pipe(gulp.dest('public'));
+});
+
+gulp.task('build', gulp.series('clean', 'styles', 'assets'));
+
+gulp.task('watch', function() {
+    gulp.watch('frontend/styles/**/*.*', gulp.series('styles'));
+    gulp.watch('frontend/assets/**/*.*', gulp.series('assets'));
+});
+
+gulp.task('server', function() {
+   browserSync.init({
+      server: 'public'
+   });
+    browserSync.watch('public/**/*.*').on('change', browserSync.reload);
+});
+
+gulp.task('dev', gulp.series('build', gulp.parallel('watch', 'server')));
